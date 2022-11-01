03784aad7f176cda61ff7e673467437c5d5edaa1
Juan Echeverry Fernandez <juan6david@gmail.com>
2022-10-31 21:51:27 -0500

    adjust functions str in class client

--- clients/models.py
+++ clients/models.py
@@ -11,4 +11,4 @@ class Client(models.Model):
     email = models.CharField(max_length=40)
 
     def __str__(self):
-        return self.nit + ' - ' + self.name
+        return str(self.id) + ' - ' + str(self.nit) + ' - ' + self.name
